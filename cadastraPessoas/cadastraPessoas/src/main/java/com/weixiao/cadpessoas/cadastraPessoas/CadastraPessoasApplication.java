32df830bd7f10bd5cc5b38fb6a4cc2b0c943f494
weixiao1994c <56464135+weixiao1994c@users.noreply.github.com>
2021-03-27 09:33:11 -0300

    iniciarProjetoDeUidade1

---
+++ cadastraPessoas/cadastraPessoas/src/main/java/com/weixiao/cadpessoas/cadastraPessoas/CadastraPessoasApplication.java
@@ -0,0 +1,13 @@
+package com.weixiao.cadpessoas.cadastraPessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastraPessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastraPessoasApplication.class, args);
+	}
+
+}
---
+++ cadastraPessoas/cadastraPessoas/src/test/java/com/weixiao/cadpessoas/cadastraPessoas/CadastraPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weixiao.cadpessoas.cadastraPessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastraPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
