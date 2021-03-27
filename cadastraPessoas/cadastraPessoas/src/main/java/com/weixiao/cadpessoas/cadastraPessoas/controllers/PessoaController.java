90bf9b7ca192a00909b4868965eaddfd8476d94c
weixiao1994c <56464135+weixiao1994c@users.noreply.github.com>
2021-03-27 18:23:22 -0300

    botarFoto

--- cadastraPessoas/cadastraPessoas/src/main/java/com/weixiao/cadpessoas/cadastraPessoas/controllers/PessoaController.java
+++ cadastraPessoas/cadastraPessoas/src/main/java/com/weixiao/cadpessoas/cadastraPessoas/controllers/PessoaController.java
@@ -1,10 +1,34 @@
 package com.weixiao.cadpessoas.cadastraPessoas.controllers;
 
+import java.security.PublicKey;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.weixiao.cadpessoas.cadastraPessoas.repositories.PessoaRepository;
 
 @Controller
 @RequestMapping ("/")
 public class PessoaController {
 
+	@Autowired
+	PessoaRepository pessoaRepo;
+	
+	@GetMapping
+	public String index() {
+		return "index.html";
+		
+	}
+	 @GetMapping("/listarPessoas")
+	 public ModelAndView listarPessoas() {
+		 list<Pessoa> list= pessoaRepo.findAll();
+		 ModelAndView mav =new ModelAndView("listarPessoas");
+				 mav.addObject("Pessoa",lista);
+				 return mav;
+				 
+	 }
+	  
 }
--- cadastraPessoas/cadastraPessoas/src/main/java/com/weixiao/cadpessoas/cadastraPessoas/repositories/PessoaRepository.java
+++ cadastraPessoas/cadastraPessoas/src/main/java/com/weixiao/cadpessoas/cadastraPessoas/repositories/PessoaRepository.java
@@ -1,6 +1,7 @@
 package com.weixiao.cadpessoas.cadastraPessoas.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.weixiao.cadpessoas.cadastraPessoas.model.Pessoa;
 
