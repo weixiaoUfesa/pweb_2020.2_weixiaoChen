141d0cf64d050e2d11cb9eb01c23a9042e6eb526
weixiao1994c <56464135+weixiao1994c@users.noreply.github.com>
2021-03-27 20:19:31 -0300

    listarpessoa

--- cadastraPessoas/cadastraPessoas/src/main/java/com/weixiao/cadpessoas/cadastraPessoas/controllers/PessoaController.java
+++ cadastraPessoas/cadastraPessoas/src/main/java/com/weixiao/cadpessoas/cadastraPessoas/controllers/PessoaController.java
@@ -1,6 +1,6 @@
 package com.weixiao.cadpessoas.cadastraPessoas.controllers;
 
-import java.security.PublicKey;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.weixiao.cadpessoas.cadastraPessoas.model.Pessoa;
 import com.weixiao.cadpessoas.cadastraPessoas.repositories.PessoaRepository;
 
 @Controller
@@ -24,9 +25,9 @@ public class PessoaController {
 	}
 	 @GetMapping("/listarPessoas")
 	 public ModelAndView listarPessoas() {
-		 list<Pessoa> list= pessoaRepo.findAll();
+		 List<Pessoa> lista = pessoaRepo.findAll();
 		 ModelAndView mav =new ModelAndView("listarPessoas");
-				 mav.addObject("Pessoa",lista);
+				 mav.addObject("pessoas",lista);
 				 return mav;
 				 
 	 }
