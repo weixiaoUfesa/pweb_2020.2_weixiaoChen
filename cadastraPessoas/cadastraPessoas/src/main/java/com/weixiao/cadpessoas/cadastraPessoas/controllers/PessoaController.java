891b25f40f037cb418a4e7ab137bdb600b0fe6a2
weixiao1994c <56464135+weixiao1994c@users.noreply.github.com>
2021-03-30 21:31:49 -0300

    remover

--- cadastraPessoas/cadastraPessoas/src/main/java/com/weixiao/cadpessoas/cadastraPessoas/controllers/PessoaController.java
+++ cadastraPessoas/cadastraPessoas/src/main/java/com/weixiao/cadpessoas/cadastraPessoas/controllers/PessoaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -39,5 +40,13 @@ public class PessoaController {
 	  @GetMapping("/adicionadaComSucesso")
 	  public String adc() {
 		  return"adicionadaComSucesso.html";
+	  }
+	  
+	  @GetMapping("/remover/{id}")
+	  public ModelAndView removerPessoa(@PathVariable("id") long id) {
+		  Pessoa aRemover = pessoaRepo.findById(id)
+				  .orElseThrow(()-> new IllegalArgumentException("ID inválido:"+id));
+		  pessoaRepo.delete(aRemover);
+		  return new ModelAndView("redirect:/listarPessoas");
 	  }
 }
